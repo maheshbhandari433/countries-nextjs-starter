c436bb4448bb8591acc1c4af3192dd8531acaadc
Bhandari Mahesh <s2300094@BC-C02GR0BSQ6LRs-MacBook-Air.local>
2024-02-09 10:28:13 +0200

    Added search

--- src/routes/Countries.jsx
+++ src/routes/Countries.jsx
@@ -1,4 +1,121 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import { Form, Spinner } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { initializeCountries } from "../store/countriesSlice";
+import { addFavourite } from "../store/favouritesSlice";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+const Countries = () => {
+  const dispatch = useDispatch();
+
+  const countriesList = useSelector((state) => state.countries.countries);
+  const loading = useSelector((state) => state.countries.isLoading);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    dispatch(initializeCountries());
+  }, [dispatch]);
+
+  useEffect(() => {
+    console.log("search: ", search);
+  }, [search]);
+
+  if (loading) {
+    return (
+      <Col className="text-center m-5">
+        <Spinner
+          animation="border"
+          role="status"
+          className="center"
+          variant="info"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </Col>
+    );
+  }
+
+  return (
+    <Container fluid>
+      <Row>
+        <Form.Control
+          style={{ width: "18rem" }}
+          type="search"
+          className="me-2 "
+          placeholder="Search for countries"
+          aria-label="Search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Row>
+      <Row xs={2} md={3} lg={4} className=" g-3">
+        {countriesList
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((country) => (
+            <Col className="mt-5" key={country.name.common}>
+              <Card className="h-100">
+                <FavoriteIcon 
+                  onClick={() => dispatch(addFavourite(country))}
+                ></FavoriteIcon>
+                <Link
+                  to={`/countries/${country.name.common}`}
+                  state={{ country: country }}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={country.flags.svg}
+                    className="rounded h-50"
+                    style={{
+                      objectFit: "cover",
+                      minHeight: "200px",
+                      maxHeight: "200px",
+                    }}
+                  />
+                </Link>
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>{country.name.common}</Card.Title>
+                  <Card.Subtitle className="mb-5 text-muted">
+                    {country.name.official}
+                  </Card.Subtitle>
+                  <ListGroup
+                    variant="flush"
+                    className="flex-grow-1 justify-content-end"
+                  >
+                    <ListGroup.Item>
+                      <i className="bi bi-translate me-2"></i>
+                      {Object.values(country.languages ?? {}).join(", ")}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <i className="bi bi-cash-coin me-2"></i>
+                      {Object.values(country.currencies || {})
+                        .map((currency) => currency.name)
+                        .join(", ")}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <i className="bi bi-people me-2"></i>
+                      {country.population.toLocaleString()}
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Countries;
+
+/* import { useEffect } from "react";
 
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { Spinner } from "react-bootstrap";
@@ -88,4 +205,4 @@ const Countries = () => {
   );
 };
 
-export default Countries;
+export default Countries; */
