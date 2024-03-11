e81786538615753be7406f625f557da3e3ead7ac
Bhandari Mahesh <s2300094@BC-C02GR0BSQ6LRs-MacBook-Air.local>
2024-03-11 10:52:40 +0200

    Added conditional rendering in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import { auth, db, logout } from "../auth/firebase";
+import { getNameOfUser } from "../auth/firebase";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const [name, setName] = useState();
+
+  useEffect(() => {
+   getNameOfUser(user).then((name) => {
+      setName(name);
+    });
+  }, [user]);
+  
   return (
     <Container fluid>
       <Row>
@@ -13,7 +28,7 @@ const Header = () => {
           <Container className="justify-content-end">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav>
+            <Nav>
                 <Link to="/">
                   <Button variant="contained">Home</Button>
                 </Link>
@@ -23,7 +38,23 @@ const Header = () => {
                 <Link to="/favourites">
                   <Button variant="contained">Favourites</Button>
                 </Link>
+                {!user && (
+                  <>
+                    <Link to="/register">
+                      <Button variant="contained">Register</Button>
+                    </Link>
+                    <Link to="/login">
+                      <Button variant="contained">Login</Button>
+                    </Link>
+                  </>
+                )}
+                {user && <Button onClick={logout}>Logout</Button>
+                }
               </Nav>
+              {/* Insert bootstrap text container here */}
+              <Navbar.Text className="m-3">
+                {name ? `Welcome, ${name}` : "Welcome, Guest"}
+              </Navbar.Text>
             </Navbar.Collapse>
           </Container>
         </Navbar>
