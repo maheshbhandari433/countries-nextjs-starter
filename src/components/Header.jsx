23c6d0ae9519bf4e611ccea78613b3b30225251c
Bhandari Mahesh <s2300094@BC-C02GR0BSQ6LRs-MacBook-Air.local>
2024-03-17 01:23:07 +0200

    Added conditional rendering of menu and name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,64 +1,56 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Row from "react-bootstrap/Row";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { auth, db, logout } from "../auth/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, logout } from "../auth/firebase";
 import { getNameOfUser } from "../auth/firebase";
 
 const Header = () => {
   const [user] = useAuthState(auth);
-
   const [name, setName] = useState();
 
   useEffect(() => {
-   getNameOfUser(user).then((name) => {
-      setName(name);
-    });
+    if (user) {
+      getNameOfUser(user).then((name) => {
+        setName(name);
+      });
+    }
   }, [user]);
-  
+
   return (
     <Container fluid>
-      <Row>
-        <Navbar bg="light" variant="light">
-          <Container className="justify-content-end">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-                <Link to="/">
-                  <Button variant="contained">Home</Button>
+      <Navbar bg="light" variant="light" expand="lg" className="justify-content-center">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/">
+              <Button variant="contained">Home</Button>
+            </Link>
+            <Link to="/countries">
+              <Button variant="contained">Countries</Button>
+            </Link>
+            <Link to="/favourites">
+              <Button variant="contained">Favourites</Button>
+            </Link>
+            {!user && (
+              <>
+                <Link to="/register">
+                  <Button variant="contained">Register</Button>
                 </Link>
-                <Link to="/countries">
-                  <Button variant="contained">Countries</Button>
+                <Link to="/login">
+                  <Button variant="contained">Login</Button>
                 </Link>
-                <Link to="/favourites">
-                  <Button variant="contained">Favourites</Button>
-                </Link>
-                {!user && (
-                  <>
-                    <Link to="/register">
-                      <Button variant="contained">Register</Button>
-                    </Link>
-                    <Link to="/login">
-                      <Button variant="contained">Login</Button>
-                    </Link>
-                  </>
-                )}
-                {user && <Button onClick={logout}>Logout</Button>
-                }
-              </Nav>
-              {/* Insert bootstrap text container here */}
-              <Navbar.Text className="m-3">
-                {name ? `Welcome, ${name}` : "Welcome, Guest"}
-              </Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </Row>
+              </>
+            )}
+          </Nav>
+          <Nav>
+            {user && <Button onClick={logout}>Logout</Button>}
+          </Nav>
+          <Navbar.Text className="m-3">
+            {name ? `Welcome, ${name}` : "Welcome, Guest"}
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
     </Container>
   );
 };
